0395ccd148f3393abdb0d8d68fb2e8bd43b48f42
Joanna Duda <duda.joann@gmail.com>
2020-06-20 10:03:23 +0200

    api

--- mediafinder/katalog/mediafinder/api.py
+++ mediafinder/katalog/mediafinder/api.py
@@ -1,3 +1,11 @@
+import requests
 
-def call_api():
-    pass
+def call_api(search_api: str, search_params: dict):
+    result = requests.get(search_api, params=search_params)
+
+    if result.status_code != 200:
+        raise Exception("Error: API request unsuccessful")
+
+    data = result.json()
+
+    return data
--- mediafinder/katalog/mediafinder/urls.py
+++ mediafinder/katalog/mediafinder/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf.urls import url, include
 from django.contrib import admin
-from .views import search_movies, register_new_user
+from .views import search_movies, register_new_user, return_search, create_rating
 
 
 urlpatterns = [
     path('search/', search_movies, name="search"),
     path('registration/', register_new_user),
-
-
+    path('my_search', return_search),
+    path('aboutus', create_rating),
 ]
